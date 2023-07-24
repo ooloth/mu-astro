e0201b13195e4c773a85a64fcaadfe6d94b36013
Michael Uloth <hello@michaeluloth.com>
2023-07-24 15:43:48 -0400

    add glow box shadow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        /* solid shadow, glow shadow */
+        glow: '0 0 0 0.2em #fda4af, 0 0 2em 0 #fda4af',
+      },
+    },
+  },
 }
