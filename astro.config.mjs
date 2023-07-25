523e1cf8447a4e757eae071d8d265d4abd1372ac
Michael Uloth <hello@michaeluloth.com>
2023-07-25 17:22:58 -0400

    update comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
-// see: https://astro.build/config
 import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
+// see: https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [tailwind(), sitemap()],
