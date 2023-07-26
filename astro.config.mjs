00ea424ddd1afc79cd8f31848f8fb6aac9ccbaaf
Michael Uloth <hello@michaeluloth.com>
2023-07-25 21:14:27 -0400

    add global styles to base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import yaml from '@rollup/plugin-yaml'
 
 // see: https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    sitemap(),
+    tailwind({
+      // Disable injecting a basic `base.css` import on every page so I can define and import my own
+      applyBaseStyles: false,
+    }),
+  ],
   scopedStyleStrategy: 'class',
   site: 'https://michaeluloth.com',
   trailingSlash: 'always',
+  vite: {
+    plugins: [yaml()],
+  },
 })
