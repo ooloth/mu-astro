0c611c348dd932deb85a172478d9c76632ead448
Michael Uloth <hello@michaeluloth.com>
2023-07-25 21:14:46 -0400

    add header component

---
+++ src/data/nav.ts
@@ -0,0 +1,19 @@
+export type NavItem = {
+  title: string
+  url: string
+}
+
+export default {
+  top: [
+    { title: 'writing', url: '/' },
+    { title: 'notes', url: '/notes/' },
+    { title: 'about', url: '/about/' },
+    // { title: 'likes', url: '/likes/' },
+  ],
+  bottom: [
+    { title: 'RSS', url: '/rss.xml' },
+    { title: 'YouTube', url: 'https://www.youtube.com/user/michaeluloth' },
+    { title: 'Twitter', url: 'https://twitter.com/ooloth' },
+    { title: 'GitHub', url: 'https://github.com/ooloth' },
+  ],
+} satisfies { top: NavItem[]; bottom: NavItem[] }
