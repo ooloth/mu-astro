49c27b40ea8485c8d58d46fec265c7da65d9bbf7
Michael Uloth <hello@michaeluloth.com>
2023-08-07 16:42:01 -0400

    extract constants

---
+++ lib/cloudinary/const.ts
@@ -0,0 +1,1 @@
+export const TRANSFORMATIONS = 'c_scale,f_auto,q_auto'
--- lib/cloudinary/insertOptimizationTransformations.ts
+++ lib/cloudinary/insertOptimizationTransformations.ts
@@ -1,3 +1,7 @@
+// TODO: use the node or js sdk to do what I'm doing here so I don't need to construct URLs?
+
+import { TRANSFORMATIONS } from './const'
+
 /**
  *
  * @param {string} cloudinaryUrl A full Cloudinary URL with no transformations (e.g. https://res.cloudinary.com/cloud_name/image/upload/version/public-id.jpg)
@@ -6,19 +10,21 @@
  * @see https://cloudinary.com/documentation/transformation_reference
  */
 function insertOptimizationTransformations(cloudinaryUrl: string, width: number): string {
-  const w = width ?? 720
-
   if (!cloudinaryUrl.includes('cloudinary')) {
     throw Error(`Not a cloudinary URL: ${cloudinaryUrl}`)
   }
+
+  const w = width ?? 720
+  const transformationsPathSegment = `${TRANSFORMATIONS},w_${w}/`
+
   if (cloudinaryUrl.includes('upload/')) {
-    return cloudinaryUrl.replace('upload/', `upload/c_scale,w_${w},f_auto,q_auto/`)
+    return cloudinaryUrl.replace('upload/', `upload/${transformationsPathSegment}/`)
   }
   if (cloudinaryUrl.includes('fetch/')) {
-    return cloudinaryUrl.replace('fetch/', `fetch/c_scale,w_${w},f_auto,q_auto/`)
+    return cloudinaryUrl.replace('fetch/', `fetch/${transformationsPathSegment}/`)
   }
   if (cloudinaryUrl.includes('youtube/')) {
-    return cloudinaryUrl.replace('youtube/', `youtube/c_scale,w_${w},f_auto,q_auto/`)
+    return cloudinaryUrl.replace('youtube/', `youtube/${transformationsPathSegment}/`)
   }
 
   return cloudinaryUrl
