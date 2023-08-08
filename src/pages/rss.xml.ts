7e5ccd2e3f77af75dbeaafd4de95ae03ee786f30
Michael Uloth <hello@michaeluloth.com>
2023-08-08 18:00:49 -0400

    parse markdown for rss using same unified plugins

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,14 +1,17 @@
 // see: https://docs.astro.build/en/guides/rss
 
 import rss from '@astrojs/rss'
-import MarkdownIt from 'markdown-it'
-import sanitizeHtml from 'sanitize-html'
 import type { APIContext } from 'astro'
+import remarkParse from 'remark-parse'
+import remarkGfm from 'remark-gfm'
+import remarkSmartyPants from 'remark-smartypants'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import { unified } from 'unified'
 
 import { getPublishedPosts } from '../utils/posts'
 import site from '../data/site'
-
-const parser = new MarkdownIt()
+import rehypeCloudinaryImageAttributes from '../../lib/rehype/cloudinary-image-attributes'
 
 export async function get(context: APIContext): Promise<{ body: string }> {
   const publishedBlogPosts = await getPublishedPosts()
@@ -19,12 +22,25 @@ export async function get(context: APIContext): Promise<{ body: string }> {
     title: site.title,
     description: site.description.rss,
     customData: `<language>en-ca</language>`,
-    items: publishedBlogPosts.map(post => ({
-      title: post.data.title,
-      pubDate: post.data.date,
-      description: post.data.description,
-      link: `/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body)),
-    })),
+    items: await Promise.all(
+      publishedBlogPosts.map(async post => {
+        const content = await unified()
+          .use(remarkParse)
+          .use(remarkGfm)
+          .use(remarkSmartyPants)
+          .use(remarkRehype)
+          .use(rehypeCloudinaryImageAttributes)
+          .use(rehypeStringify)
+          .process(post.body)
+
+        return {
+          title: post.data.title,
+          pubDate: post.data.date,
+          description: post.data.description,
+          link: `/${post.slug}/`,
+          content: String(content.value),
+        }
+      }),
+    ),
   })
 }
