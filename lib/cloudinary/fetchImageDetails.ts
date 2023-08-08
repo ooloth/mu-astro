eaf2c0b095939bdfa24c92dad64ba5362785ac9b
Michael Uloth <hello@michaeluloth.com>
2023-08-07 21:22:29 -0400

    look for custom attributes as query params

--- lib/cloudinary/fetchImageDetails.ts
+++ lib/cloudinary/fetchImageDetails.ts
@@ -1,10 +1,13 @@
 import cloudinary from './client'
 import isCloudinaryImage from './isCloudinaryImage'
 
-async function fetchImageDetails(publicId: string): Promise<unknown> {
+async function fetchImageDetails(publicId: string) {
   if (!isCloudinaryImage(publicId)) {
     throw Error(`${publicId} is not a Cloudinary image path.`)
   }
+
+  const customAttributes = new URL(publicId, 'https://res.cloudinary.com').searchParams
+  console.log('customAttributes', customAttributes)
 
   // see: https://cloudinary.com/documentation/admin_api#using_sdks_with_the_admin_api
   // see: https://cloudinary.com/documentation/admin_api#get_details_of_a_single_resource_by_public_id
