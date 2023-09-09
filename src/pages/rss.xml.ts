cbfbd9982ff87eae8a76b2406f331d0f0595309b
Michael Uloth <hello@michaeluloth.com>
2023-09-08 22:51:27 -0400

    add missing remark and rehype plugins to rss pipeline

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,6 +6,8 @@ import remarkParse from 'remark-parse'
 import remarkGfm from 'remark-gfm'
 import remarkSmartyPants from 'remark-smartypants'
 import remarkRehype from 'remark-rehype'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeStringify from 'rehype-stringify'
 import { unified } from 'unified'
 
@@ -26,12 +28,25 @@ export async function get(context: APIContext): Promise<{ body: string }> {
     customData: `<language>en-ca</language><lastBuildDate>${new Date().toUTCString()}</lastBuildDate>`,
     items: await Promise.all(
       publishedBlogPosts.map(async post => {
+        // Match the remark and rehype plugins used in astro.config.mjs
         const content = await unified()
           .use(remarkParse)
           .use(remarkGfm)
           .use(remarkSmartyPants)
+          .use(remarkUnwrapImages)
           .use(remarkRehype)
           .use(rehypeCloudinaryImageAttributes) // mine
+          .use(rehypePrettyCode, {
+            // see: https://rehype-pretty-code.netlify.app
+            tokensMap: {
+              fn: 'entity.name.function',
+              kw: 'keyword',
+              key: 'meta.object-literal.key',
+              pm: 'variable.parameter',
+              obj: 'variable.other.object',
+              str: 'string',
+            },
+          })
           .use(rehypeStringify)
           .process(post.body)
 
