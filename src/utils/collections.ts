08c0edb94d2c04ad4dd7285d2c8ee2238539a282
Michael Uloth <hello@michaeluloth.com>
2024-11-11 23:48:23 -0500

    collections: recognize til entries

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,12 +1,13 @@
 import type { CollectionEntry } from 'astro:content'
 
 export type Draft = CollectionEntry<'drafts'>
+export type TIL = CollectionEntry<'til'>
 export type Writing = CollectionEntry<'writing'>
 
 /**
  * Returns true if the pathname matches any slug in the collection (at any ancestry level)
  */
-export const isPathnameInCollection = (pathname: string, collection: Writing[] | Draft[]): boolean => {
+export const isPathnameInCollection = (pathname: string, collection: Writing[] | Draft[] | TIL[]): boolean => {
   const pathnameMatchesSlug = (slug: string): boolean => pathname === `/${slug}/`
 
   return collection.some(
