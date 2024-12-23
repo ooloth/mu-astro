213abd2d936791073b41fea6d97f2e8aaff81ee9
Michael Uloth <hello@michaeluloth.com>
2024-12-22 22:26:47 -0500

    tags: find matching entries

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -1,10 +1,44 @@
+import type { Bookmark, Draft, TIL, Writing } from './collections'
+import type { Post } from './posts'
+
 export const cleanTags = (tags?: string[]): string[] =>
   Array.from(
     new Set(
       (tags ?? [])
         .filter(Boolean)
-        .filter(tag => !['note'].includes(tag))
+        .filter(tag => !['bookmark', 'note', 'post', 'til'].includes(tag))
+        .map(tag => tag.replace('s/', ''))
+        .map(tag => tag.replace('t/', ''))
         .map(tag => tag.replace('topic/', ''))
         .map(tag => tag.replaceAll('-', ' ')),
     ),
   ).sort()
+
+/**
+ * Returns a mapping of the entry's tags to lists of other content entries with that tag.
+ */
+export const getEntriesWithTags = async (
+  entry: Bookmark | Draft | TIL | Post | Writing,
+  collections: (Bookmark | Draft | TIL | Post | Writing)[],
+): Promise<Record<string, (Bookmark | Draft | TIL | Post | Writing)[]>> => {
+  const relatedByTag: Record<string, (Bookmark | Draft | TIL | Post | Writing)[]> = {}
+
+  for (const item of collections) {
+    // Go in order of the entry's tags, which are presumably sorted from most to least relevant
+    for (const tag of cleanTags(entry.data.tags) ?? []) {
+      if ((item.data.tags ?? []).includes(tag)) {
+        if (item.data.title === entry.data.title) {
+          continue
+        }
+
+        if (!relatedByTag[tag]) {
+          relatedByTag[tag] = []
+        }
+
+        relatedByTag[tag].push(item)
+      }
+    }
+  }
+
+  return relatedByTag
+}
