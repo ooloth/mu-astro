5dd70e8438789be9eb9ee725e6d7521dbdca1525
Michael Uloth <hello@michaeluloth.com>
2024-12-27 13:59:13 -0500

    tils: resolve schema-related type errors

--- src/utils/tils.ts
+++ src/utils/tils.ts
@@ -1,48 +1,43 @@
-import { getCollection } from 'astro:content'
+import { type MarkdownHeading } from 'astro'
+import { type AstroComponentFactory } from 'astro/runtime/server/index.js'
+import { getCollection, render } from 'astro:content'
 
-import type { Draft, TIL } from './collections'
+import { type Draft, type TIL } from './collections'
 
 type TILWithContent = TIL & { Content: AstroComponentFactory; headings: MarkdownHeading[] }
-type TILWithDate = TIL & { data: { date: string } }
 
-export const isTIL = (til: TIL): til is TIL => til.data.tags?.includes('til')
+export const isTIL = (til: TIL): til is TIL => (til.data.tags ?? []).includes('til')
 
 /**
  * Returns true if TIL is published.
  */
-const isPublished = (til: TIL): til is TILWithDate =>
-  isTIL(til) && til.data.date && Date.parse(til.data.date) <= Date.now()
+const isPublished = (til: TIL): boolean => isTIL(til) && til.data.date && til.data.date.getTime() <= Date.now()
 
 /**
  * Sorts two TILs in descending order by publish date (or the current date if either TIL has no date yet).
  */
 const sortByDate = (a: TIL | Draft, b: TIL | Draft): number =>
-  (Date.parse(b.data.date) || Date.now()).toString().localeCompare((Date.parse(a.data.date) || Date.now()).toString())
+  (b.data.date || Date.now()).toString().localeCompare((a.data.date || Date.now()).toString())
 
 /**
  * Returns TILs sorted in descending order by publish date.
  */
-const sortTILs = (tils: TIL[]): TILWithDate[] => tils.sort(sortByDate)
-
-import { getEntry } from 'astro:content'
-import type { MarkdownHeading } from 'astro'
-import type { AstroComponentFactory } from 'astro/dist/runtime/server'
+const sortTILs = (tils: TIL[]): TIL[] => tils.sort(sortByDate)
 
 /**
  * Returns all published TILs, in descending order by date (useful for RSS feed).
  */
-export const getPublishedTILs = async (): Promise<TILWithDate[]> => sortTILs(await getCollection('til', isPublished))
+export const getPublishedTILs = async (): Promise<TIL[]> => sortTILs(await getCollection('tils', isPublished))
 
 /**
  * Returns all TILs in development and only published posts in production, in descending order by date.
  */
 export const getTILs = async (): Promise<TILWithContent[]> => {
-  const tilProperties = await getCollection('til', til => (import.meta.env.PROD ? isPublished(til) : isTIL(til)))
+  const tilProperties = await getCollection('tils', til => (import.meta.env.PROD ? isPublished(til) : isTIL(til)))
 
   const tilsWithContent: TILWithContent[] = await Promise.all(
     tilProperties.map(async til => {
-      const entry = await getEntry('til', til.id)
-      const { Content, headings, remarkPluginFrontmatter } = await entry!.render() // TODO: make safer
+      const { Content, headings, remarkPluginFrontmatter } = await render(til) // TODO: make safer
       return { ...til, Content, headings, data: { ...til.data, ...remarkPluginFrontmatter } }
     }),
   )
