008a83eb9feee4e1ec6273e047fd1114c7e63469
Michael Uloth <hello@michaeluloth.com>
2024-12-27 16:34:05 -0500

    fix import

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -11,6 +11,8 @@ const getHumanReadableDate = (date: string | number | Date): string =>
 /**
  * Given a date, returns a machine-readable date string suitable for use in the `datetime` attribute of the `<time>` element.
  * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time#valid_datetime_values
+ *
+ * WARN: This errors if passed an undefined date.
  */
 const getMachineReadableDate = (date: string | number | Date): string => new Date(date).toISOString()
 
