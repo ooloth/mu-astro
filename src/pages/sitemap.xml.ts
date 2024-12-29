2d7487d4462453b5ef70abda24ebbdd64806c7be
Michael Uloth <hello@michaeluloth.com>
2024-12-28 20:00:06 -0500

    sitemap: include til and bookmark routes

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,14 +3,24 @@
 
 import { getPosts } from '../utils/posts'
 import { getNotes } from '../utils/notes'
+import { getTILs } from '../utils/tils'
+import { getBookmarks } from '../utils/bookmarks'
 
 export async function GET(): Promise<Response> {
-  const pages = ['/', '/notes/', '/about/', '/likes/'] // static pages statically output by src/pages
-  const posts = await getPosts() // posts dynamically output by src/pages/[slug].astro (excluding drafts in production)
-  const notes = await getNotes() // notes dynamically output by src/pages/[slug].astro (excluding private notes in production)
+  const pages = ['/', '/til/', '/notes/', '/about/', '/likes/'] // static pages statically output by src/pages
+  const posts = await getPosts() // posts dynamically output by src/pages/[slug].astro
+  const tils = await getTILs() // notes dynamically output by src/pages/[slug].astro
+  const notes = await getNotes() // notes dynamically output by src/pages/[slug].astro
+  const bookmarks = await getBookmarks() // notes dynamically output by src/pages/[slug].astro
 
   // all paths in /[slug]/ format
-  const allPaths = [...pages, ...posts.map(post => `/${post.id}/`), ...notes.map(note => `/${note.id}/`)]
+  const allPaths = [
+    ...pages,
+    ...posts.map(post => `/${post.id}/`),
+    ...tils.map(til => `/${til.id}/`),
+    ...notes.map(note => `/${note.id}/`),
+    ...bookmarks.map(bookmark => `/${bookmark.id}/`),
+  ]
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${allPaths
     .map(path => `<url><loc>${`https://michaeluloth.com${path}`}</loc></url>`)
