1ec59b3da00e5983a4668fd8c334d9d9ae065466
Michael Uloth <hello@michaeluloth.com>
2024-12-29 15:22:55 -0500

    remove tags: include tags at the start of a text segment (e.g. on their own line)

--- lib/remark/remove-tags.ts
+++ lib/remark/remove-tags.ts
@@ -2,26 +2,21 @@
 // see: https://github.com/syntax-tree/mdast
 
 import type { RemarkPlugin } from '@astrojs/markdown-remark'
-import type { Data, Text } from 'mdast'
-import type { Node } from 'unist'
+import type { Root, Text } from 'mdast'
 import { visit } from 'unist-util-visit'
-// import { type Transformer } from 'unified'
 
 /**
  * Given a string, remove all topic tags like " #post" or " #question"
  * TODO: can I move these tags to the frontmatter?
  */
-const removeTags = (str: string): string => str.replaceAll(/\s#[\w|/]+/g, '')
+const removeTags = (str: string): string => str.replaceAll(/#[\w|/]+/g, '')
 
-type Transformer = (tree: Node<Data>) => Promise<void>
-
-// TODO: is this working? I thought I saw inline tags still there...
-const remarkRemoveTags: RemarkPlugin =
-  (): Transformer =>
-  async (tree: Node<Data>): Promise<void> => {
-    // identify the type of node I want to modify ("text" in this case) here: https://astexplorer.net
+const remarkRemoveTags =
+  (): RemarkPlugin =>
+  (tree: Root): void => {
+    // Identify the type of node I want to modify ("text" in this case) here: https://astexplorer.net
     visit(tree, 'text', (node: Text): void => {
-      if (!node.value.includes(' #')) return
+      if (!node.value.includes('#')) return
 
       // Use Object.assign to replace the exact same object instead of triggering an infinite loop by creating new objects
       Object.assign(node, {
