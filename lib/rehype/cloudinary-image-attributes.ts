ff3259b488b05d3e1b9cad9cf527234bb7d03aae
Michael Uloth <hello@michaeluloth.com>
2024-12-29 15:10:50 -0500

    cloudinary image attributes: don't reprocess processed images

--- lib/rehype/cloudinary-image-attributes.ts
+++ lib/rehype/cloudinary-image-attributes.ts
@@ -1,15 +1,14 @@
-// see: https://www.ryanfiller.com/blog/remark-and-rehype-plugins
-// see: https://github.com/bradgarropy/rehype-cloudinary-image-size/blob/master/src/rehypeCloudinaryImageSize.ts
-// see: https://github.com/bradgarropy/rehype-cloudinary-image-size/blob/master/example/index.js
+// See: https://unifiedjs.com/learn/guide/create-a-rehype-plugin/
+// See: https://www.ryanfiller.com/blog/remark-and-rehype-plugins
+// See: https://github.com/bradgarropy/rehype-cloudinary-image-size/blob/master/src/rehypeCloudinaryImageSize.ts
+// See: https://github.com/bradgarropy/rehype-cloudinary-image-size/blob/master/example/index.js
 
 import type { RehypePlugin } from '@astrojs/markdown-remark'
-import type { Properties } from 'hast'
+import type { Element, Properties, Root } from 'hast'
 import { isElement } from 'hast-util-is-element'
-import type { Element } from 'hast'
 import { CONTINUE, SKIP, visit } from 'unist-util-visit'
-import type { Node } from 'unist'
 
-import isCloudinaryUpload from '../cloudinary/isCloudinaryUpload'
+import isCloudinaryMuImage from '../cloudinary/isCloudinaryUpload'
 import cloudinary from '../cloudinary/client'
 import findCachedResourceByPublicId from '../cloudinary/findPrefetchedResourceByPublicId'
 
@@ -17,29 +16,20 @@ interface Image extends Element {
   properties: Properties
 }
 
-const rehypeCloudinaryImageAttributes: RehypePlugin = () => {
+const rehypeCloudinaryImageAttributes = (): RehypePlugin => {
   const htmlImages: Image[] = []
 
   const visitor = (node: Element) => {
-    if (
-      isElement(node, 'img') &&
-      node.properties !== undefined &&
-      typeof node.properties.src === 'string' &&
-      isCloudinaryUpload(node.properties.src)
-    ) {
-      htmlImages.push(node as Image)
+    if (isElement(node, 'img') && isCloudinaryMuImage(node?.properties?.src)) {
+      htmlImages.push(node)
       return SKIP
     }
 
     return CONTINUE
   }
 
-  const transformer = async (tree: Node) => {
+  const transformer = (tree: Root): void => {
     visit(tree, 'element', visitor)
-
-    const cloudinaryImageDetails = htmlImages.map(htmlImage =>
-      findCachedResourceByPublicId(htmlImage.properties?.src as string),
-    )
 
     const widths = [
       350, // image layout width on phone at 1x DPR
@@ -54,17 +44,20 @@ const rehypeCloudinaryImageAttributes: RehypePlugin = () => {
     ]
 
     // Mutate the image nodes with the image details
-    htmlImages.forEach((htmlImage, index) => {
-      const cloudinaryDetails = cloudinaryImageDetails[index]
+    htmlImages.forEach(htmlImage => {
+      if (!isCloudinaryMuImage(htmlImage.properties?.src as string)) {
+        return // skip images I've already processed (this is a workaround for bug on my part)
+      }
+      const cloudinaryDetails = findCachedResourceByPublicId(htmlImage.properties?.src as string)
 
-      htmlImage.properties.src = cloudinary.url(cloudinaryDetails.public_id, {
+      const newSrc = cloudinary.url(cloudinaryDetails.public_id, {
         crop: 'scale',
         fetch_format: 'auto',
         quality: 'auto',
         width: 1440,
       })
 
-      htmlImage.properties.srcset = widths
+      const newSrcset = widths
         .map(
           width =>
             `${cloudinary.url(cloudinaryDetails.public_id, {
@@ -76,28 +69,39 @@ const rehypeCloudinaryImageAttributes: RehypePlugin = () => {
         )
         .join(', ')
 
-      // For blog posts and notes, image layout size currently maxes out when browser hits 768px
-      // NOTE: browser takes first media query that's true, so be careful about the order
-      htmlImage.properties.sizes = '(min-width: 768px) 768px, 100vw'
+      const newSizes = '(min-width: 768px) 768px, 100vw'
 
       // @ts-expect-error: "custom" property currently defined as type "object"
-      htmlImage.properties.alt = cloudinaryDetails?.context?.custom?.alt ?? ' ' // comes from "Description" field in contextual metadata
-      htmlImage.properties.width = cloudinaryDetails.width
-      htmlImage.properties.height = cloudinaryDetails.height
-      htmlImage.properties.loading = cloudinaryDetails.loading
-      htmlImage.properties.decoding = cloudinaryDetails.decoding
+      const newAlt = cloudinaryDetails?.context?.custom?.alt ?? ' ' // comes from "Description" field in contextual metadata
+      const newWidth = cloudinaryDetails.width
+      const newHeight = cloudinaryDetails.height
+      const newLoading = cloudinaryDetails.loading
+      const newDecoding = cloudinaryDetails.decoding
 
       // @ts-expect-error: "custom" property currently defined as type "object"
       const caption = cloudinaryDetails?.context?.custom?.caption // comes from "Title" field in contextual metadata
 
       if (caption) {
-        // see: https://github.com/syntax-tree/hast#element
+        // Create a new figure element
         const figure = {
           type: 'element',
           tagName: 'figure',
           properties: {},
           children: [
-            { ...htmlImage },
+            {
+              ...htmlImage,
+              properties: {
+                ...htmlImage.properties,
+                src: newSrc,
+                srcset: newSrcset,
+                sizes: newSizes,
+                alt: newAlt,
+                width: newWidth,
+                height: newHeight,
+                loading: newLoading,
+                decoding: newDecoding,
+              },
+            },
             {
               type: 'element',
               tagName: 'figcaption',
@@ -106,9 +110,18 @@ const rehypeCloudinaryImageAttributes: RehypePlugin = () => {
           ],
         }
 
-        // If a caption exists, replace the image with a figure that contains the img + figcaption
-        // Use Object.assign to replace the exact same object instead of triggering an infinite loop by creating new objects
+        // Replace the image node with the figure node
         Object.assign(htmlImage, figure)
+      } else {
+        // Mutate the image node directly
+        htmlImage.properties.src = newSrc
+        htmlImage.properties.srcset = newSrcset
+        htmlImage.properties.sizes = newSizes
+        htmlImage.properties.alt = newAlt
+        htmlImage.properties.width = newWidth
+        htmlImage.properties.height = newHeight
+        htmlImage.properties.loading = newLoading
+        htmlImage.properties.decoding = newDecoding
       }
     })
   }
