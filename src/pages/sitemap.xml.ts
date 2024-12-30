1ba37905b17e54d0e2b70b67b810ce4e795678b3
Michael Uloth <hello@michaeluloth.com>
2024-12-29 20:35:29 -0500

    sitemap: add draft pages

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,23 +1,25 @@
 // @astrojs/sitemap doesn't allow filtering content collection items by their data properties, so I wrote this simple static file endpoint instead.
 // see: https://docs.astro.build/en/core-concepts/endpoints/#static-file-endpoints
 
-import { getPosts } from '../utils/posts'
+import { getDrafts, getPosts } from '../utils/posts'
 import { getNotes } from '../utils/notes'
 import { getTILs } from '../utils/tils'
 import { getBookmarks } from '../utils/bookmarks'
 
 export async function GET(): Promise<Response> {
   const pages = ['/', '/til/', '/notes/', '/about/', '/likes/'] // static pages statically output by src/pages
-  const posts = await getPosts() // posts dynamically output by src/pages/[slug].astro
-  const tils = await getTILs() // notes dynamically output by src/pages/[slug].astro
-  const notes = await getNotes() // notes dynamically output by src/pages/[slug].astro
-  const bookmarks = await getBookmarks() // notes dynamically output by src/pages/[slug].astro
+  const posts = await getPosts() // pages dynamically created by src/pages/[slug].astro
+  const tils = await getTILs() // pages dynamically created by src/pages/[slug].astro
+  const drafts = await getDrafts() // pages dynamically created by src/pages/[slug].astro
+  const notes = await getNotes() // pages dynamically created by src/pages/[slug].astro
+  const bookmarks = await getBookmarks() // pages dynamically created by src/pages/[slug].astro
 
   // all paths in /[slug]/ format
   const allPaths = [
     ...pages,
     ...posts.map(post => `/${post.id}/`),
     ...tils.map(til => `/${til.id}/`),
+    ...drafts.map(draft => `/${draft.id}/`),
     ...notes.map(note => `/${note.id}/`),
     ...bookmarks.map(bookmark => `/${bookmark.id}/`),
   ]
