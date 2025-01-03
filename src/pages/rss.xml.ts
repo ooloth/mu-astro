be69e13b0a00ff1151d9798d9013b4b8f55af0b1
Michael Uloth <hello@michaeluloth.com>
2025-01-02 23:38:33 -0500

    rss: use my name as the author (instead of automatically using my gmail address somehow)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -59,6 +59,7 @@ export async function GET(context: APIContext): Promise<Response> {
           title: post.data.title ?? undefined,
           link: slug,
           description: post.data.description ?? undefined,
+          author: site.author.name,
           pubDate: post.data.date ?? undefined,
           customData: `<guid permalink="true">${permalink}</guid><author>${site.author.email})</author>`,
           content: cleanContent(String(content.value)),
